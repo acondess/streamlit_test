fdf565b65ef290c6e0e7cc6ea63d401fa6e794a6
acondess <acondess@qq.com>
2023-06-12 15:58:49 +0800

    Update st_write.py
    add code write

--- st_write.py
+++ st_write.py
@@ -33,3 +33,9 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+# 样例 6
+    # 输出代码
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
