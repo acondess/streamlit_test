249f901e34d34f0be16459e1695c146babe1e113
acondess <acondess@qq.com>
2023-06-13 13:57:16 +0800

    Create st_selectbox.py
    add selectbox

---
+++ st_selectbox.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+code = f'''
+import streamlit as st
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+'''
+
+st.code(code,language='python')
