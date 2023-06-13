ca8e985776b52ddb1a01341b4873084cdc795b31
acondess <acondess@qq.com>
2023-06-13 10:08:28 +0800

    Update st_slider.py
    slider demo 3&4

--- st_slider.py
+++ st_slider.py
@@ -5,35 +5,35 @@ st.header('st.slider')
 
 # 样例 1
 
-st.subheader('Slider')
+st.subheader('Slider 滑动条')
 
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
 
 # 样例 2
 
-st.subheader('Range slider')
+st.subheader('Range slider 范围滑动条')
 
 values = st.slider(
      'Select a range of values',
      0.0, 100.0, (25.0, 75.0))
 st.write('Values:', values)
 
-# # 样例 3
+# 样例 3
 
-# st.subheader('Range time slider')
+st.subheader('Range time slider 时间范围滑动条')
 
-# appointment = st.slider(
-#      "Schedule your appointment:",
-#      value=(time(11, 30), time(12, 45)))
-# st.write("You're scheduled for:", appointment)
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
 
-# # 样例 4
+# 样例 4
 
-# st.subheader('Datetime slider')
+st.subheader('Datetime slider 日期滑动条')
 
-# start_time = st.slider(
-#      "When do you start?",
-#      value=datetime(2020, 1, 1, 9, 30),
-#      format="MM/DD/YY - hh:mm")
-# st.write("Start time:", start_time)
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
